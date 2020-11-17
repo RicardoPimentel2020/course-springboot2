364178e81fff9177f2c88c3556b8253cb40cb5be
RicardoPimentel2020 <ricardopimentel27@gmail.com>
2020-11-17 13:21:03 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/alphasystemcourse/entities/User.java
+++ src/main/java/com/alphasystemcourse/entities/User.java
@@ -11,10 +11,10 @@ import javax.persistence.Id;
 public class User implements Serializable  {
 	
 	private static final long serialVersionUID = 1L;
-	/*auto incremento*/
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -24,7 +24,7 @@ public class User implements Serializable  {
 		
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -33,11 +33,11 @@ public class User implements Serializable  {
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -77,7 +77,7 @@ public class User implements Serializable  {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -90,10 +90,14 @@ public class User implements Serializable  {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
+
 	
 	
 }
---
+++ src/main/java/com/alphasystemcourse/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.alphasystemcourse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alphasystemcourse.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
