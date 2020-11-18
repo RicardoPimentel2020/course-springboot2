883d78d1040e992a85cf273b3324d4a7c8e01e0a
RicardoPimentel2020 <ricardopimentel27@gmail.com>
2020-11-18 13:49:35 -0300

    OrderStatus enum

--- src/main/java/com/alphasystemcourse/entities/Order.java
+++ src/main/java/com/alphasystemcourse/entities/Order.java
@@ -11,68 +11,73 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.alphasystemcourse.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-@Table(name = "tb_order")/* noome da tabela que será criada no banco de dados */
+@Table(name = "tb_order") /* noome da tabela que será criada no banco de dados */
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	@ManyToOne /* define o relacionamento entre tabela User muitos para 1*/
+
+	private Integer orderStatus;
+
+	@ManyToOne /* define o relacionamento entre tabela User muitos para 1 */
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	
+
 	public Order() {
-		
+
 	}
 
-
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public Instant getMoment() {
 		return moment;
 	}
 
-
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 	public User getClient() {
 		return client;
 	}
 
-
 	public void setClient(User client) {
 		this.client = client;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -81,7 +86,6 @@ public class Order implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -99,6 +103,5 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
